8cc9fe4406455a27d40dda3f755909d967b672b8
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2015-07-27 17:10:12 -0500

    adding blink example code to first_sketch

--- sketchbook_eclipse/first_sketch/first_sketch.cpp
+++ sketchbook_eclipse/first_sketch/first_sketch.cpp
@@ -1,15 +1,26 @@
-// Do not remove the include below
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+
+  This example code is in the public domain.
+ */
 #include "first_sketch.h"
 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
 
-//The setup function is called once at startup of the sketch
-void setup()
-{
-// Add your initialization code here
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
 }
 
-// The loop function is called in an endless loop
-void loop()
-{
-//Add your repeated code here
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
 }
