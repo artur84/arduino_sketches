6be72f43995006291a939fa1069c231214130cfa
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2015-07-27 23:57:56 -0500

    change ros_dc_motors to use my customized functions instead of those from the AFMotor.h library

--- sketchbook_eclipse/ros_dc_motor/ros_dc_motor.cpp
+++ sketchbook_eclipse/ros_dc_motor/ros_dc_motor.cpp
@@ -2,7 +2,7 @@
 #include "ros_dc_motor.h"
 /*
  * This program hears a Twist message in a ROS topic
- * and commands a motor according to the linear.x
+ * and commands two motors according to the linear.x
  * value of the received Twist.
  * mantainer: arturoescobedo.iq@gmail.com
  */
@@ -10,53 +10,124 @@
 /*
  * Global variables
  */
-ros::NodeHandle  nh;
-AF_DCMotor motor(1, MOTOR12_64KHZ); // create motor #1, 64KHz pwm
-
+ros::NodeHandle nh;
 
 /*
  * My functions
  */
+/* Moves the left wheel forward
+ * motor: LEFT or RIGHT (0 or 1)
+ * dir: FORWARD or BACKWARD (+1 or -1) to choose the motor direction
+ * speed: a value between 0 and 255
+ */
+void move_motor(int motor, int dir, int speed) {
+	if (motor == LEFT) {
+		if (dir == FORWARD) {
+			digitalWrite(LEFT_MOT_POS, 1);
+			digitalWrite(LEFT_MOT_NEG, 0);
+			analogWrite(LEFT_MOT_EN, speed);
+		} else if (dir == BACKWARD) {
+			digitalWrite(LEFT_MOT_POS, 0);
+			digitalWrite(LEFT_MOT_NEG, 1);
+			analogWrite(LEFT_MOT_EN, speed);
+		} else {
+			//Stop if received an wrong direction
+			digitalWrite(LEFT_MOT_POS, 0);
+			digitalWrite(LEFT_MOT_NEG, 0);
+			analogWrite(LEFT_MOT_EN, 0);
+		}
+
+	} else if (motor == RIGHT) {
+		if (dir == FORWARD) {
+			digitalWrite(RIGHT_MOT_POS, 1);
+			digitalWrite(RIGHT_MOT_NEG, 0);
+			analogWrite(RIGHT_MOT_EN, speed);
+		} else if (dir == BACKWARD) {
+			digitalWrite(RIGHT_MOT_POS, 0);
+			digitalWrite(RIGHT_MOT_NEG, 1);
+			analogWrite(RIGHT_MOT_EN, speed);
+		} else {
+			//Stop if received an wrong direction
+			digitalWrite(RIGHT_MOT_POS, 0);
+			digitalWrite(RIGHT_MOT_NEG, 0);
+			analogWrite(RIGHT_MOT_EN, 0);
+		}
+
+	}
+
+}
+
+/* Hard stop of motor
+ * motor: LEFT or RIGHT (0 or 1)
+ */
+void hard_stop(int motor) {
+	//Stop if received an wrong direction
+	if (motor == LEFT) {
+		digitalWrite(LEFT_MOT_POS, 1);
+		digitalWrite(LEFT_MOT_NEG, 1);
+		analogWrite(LEFT_MOT_EN, 255);
+	} else if (motor == RIGHT) {
+		//Stop if received an wrong direction
+		digitalWrite(RIGHT_MOT_POS, 1);
+		digitalWrite(RIGHT_MOT_NEG, 1);
+		analogWrite(RIGHT_MOT_EN, 255);
+	}
+}
+
+/* Soft stop of motor
+ * motor: LEFT or RIGHT (0 or 1)
+ */
+void soft_stop(int motor) {
+	//Stop if received an wrong direction
+	if (motor == LEFT) {
+		digitalWrite(LEFT_MOT_POS, 0);
+		digitalWrite(LEFT_MOT_NEG, 0);
+		analogWrite(LEFT_MOT_EN, 0);
+	} else if (motor == RIGHT) {
+		//Stop if received an wrong direction
+		digitalWrite(RIGHT_MOT_POS, 0);
+		digitalWrite(RIGHT_MOT_NEG, 0);
+		analogWrite(RIGHT_MOT_EN, 0);
+	}
+}
 
 //Twist callback
-void cmd_vel_cb( const geometry_msgs::Twist& cmd_msg){
-   digitalWrite(13, HIGH-digitalRead(13));  //toggles a led
-   float x = cmd_msg.linear.x;
-   int motor_speed = 55 + 50*x;    //linear x should be maximum 4
-   motor.setSpeed(motor_speed); // set the speed according to linear.x
-   if ((x<0) && (x >= -4)){
-     motor.run(BACKWARD); // turn it on going backward
-   }
-   else if((x >0) && (x<= 4)){
-     motor.run(FORWARD); // turn it on going forward
-   }
-   else{//Stop if 0 or a value out of boundaries is sent
-     motor_speed=0;
-     motor.setSpeed(motor_speed); // set the speed
-     motor.run(RELEASE); // stop it
-   }
+void cmd_vel_cb(const geometry_msgs::Twist& cmd_msg) {
+	digitalWrite(13, HIGH - digitalRead(13));  //toggles a led
+	float x = cmd_msg.linear.x;
+	int motor_speed = 55 + 50 * x;    //linear x should be maximum 4
+
+	if ((x < 0) && (x >= -4)) {
+		move_motor(LEFT, BACKWARD, motor_speed); // turn it on going backward
+		move_motor(RIGHT, BACKWARD, motor_speed); // turn it on going backward
+	} else if ((x > 0) && (x <= 4)) {
+		move_motor(LEFT, FORWARD, motor_speed); // turn it on going forward
+		move_motor(RIGHT, FORWARD, motor_speed); // turn it on going forward
+	} else { //Stop if 0 or a value out of boundaries is sent
+		motor_speed = 0;
+		hard_stop(LEFT);
+		soft_stop(RIGHT);
+	}
 }
 
 //Creates the ROS subscriber
 ros::Subscriber<geometry_msgs::Twist> sub("arduino/cmd_vel", cmd_vel_cb);
 
-
 /*
  * Arduino SETUP
  */
 void setup() {
-  pinMode(13, OUTPUT);
-  nh.initNode();
-  nh.subscribe(sub);
-  motor.setSpeed(200); // set the speed to 200/255
+	pinMode(13, OUTPUT);
+	nh.initNode();
+	nh.subscribe(sub);
 }
 
 /*
  * Arduino MAIN LOOP
  */
 void loop() {
-  //I will just keep the loop waiting for a message
-  //in the ros topic
-  nh.spinOnce();
-  delay(1);
+	//I will just keep the loop waiting for a message
+	//in the ros topic
+	nh.spinOnce();
+	delay(1);
 }
--- sketchbook_eclipse/ros_dc_motor/ros_dc_motor.h
+++ sketchbook_eclipse/ros_dc_motor/ros_dc_motor.h
@@ -7,12 +7,23 @@
 #ifndef _ros_dc_motor_H_
 #define _ros_dc_motor_H_
 #include "Arduino.h"
-
-#include <AFMotor.h>
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 //add your includes for the project ros_dc_motor here
 
+/*******************************
+ * Define the material
+ ******************************/
+#define RIGHT_MOT_POS 2
+#define RIGHT_MOT_NEG 3
+#define RIGHT_MOT_EN A2 //enable
+#define LEFT_MOT_POS 4
+#define LEFT_MOT_NEG 5
+#define LEFT_MOT_EN A3 //enable
+#define BACKWARD -1  //moves backwards
+#define FORWARD 1
+#define LEFT 1
+#define RIGHT 0
 
 //end of add your includes here
 #ifdef __cplusplus
@@ -25,9 +36,6 @@ void setup();
 #endif
 
 //add your function definitions for the project ros_dc_motor here
-
-
-
 
 //Do not add code below this line
 #endif /* _ros_dc_motor_H_ */
