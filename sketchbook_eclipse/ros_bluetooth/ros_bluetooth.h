3a367e014f038787369401a05716d04c8e022d01
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2015-07-27 23:01:01 -0500

    Adding ros_bluetooth project

---
+++ sketchbook_eclipse/ros_bluetooth/ros_bluetooth.h
@@ -0,0 +1,34 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _ros_bluetooth_H_
+#define _ros_bluetooth_H_
+//#define USE_USBCON //Comment this line if you are using Arduino UNO, or leonardo with bluetooth.
+#include "Arduino.h"
+#include <ros.h>
+#include <std_msgs/String.h>
+
+
+//add your includes for the project ros_bluetooth here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project ros_bluetooth here
+
+
+
+
+//Do not add code below this line
+#endif /* _ros_bluetooth_H_ */
