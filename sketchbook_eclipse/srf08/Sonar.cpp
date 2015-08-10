a06de3733468e543075950fdce9af97bd35cc103
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2015-08-09 21:52:35 -0500

    Now all the registers and addresses are manged inside the class

--- sketchbook_eclipse/srf08/Sonar.cpp
+++ sketchbook_eclipse/srf08/Sonar.cpp
@@ -15,10 +15,15 @@ Sonar::Sonar() {
 Sonar::~Sonar() {
 
 }
-
+/* Connects to the sonar
+ *
+ * Call this function in the arduino setup()
+ */
 void Sonar::connect() {
 	// start I2C bus
 	Wire.begin();
+	delay(10);
+	changeAddress (address_);	//Don't delete this!! somehow is necessary
 }
 
 /* Communicates with Sonar to send commands
@@ -36,24 +41,23 @@ void Sonar::sendCommand(int address, int commandRegister, int command) {
 }
 
 /*
- * Starts a measurement, don't forget to wait at least 70ms
- * before calling readResult()
+ * Starts a measurement,
+ * don't forget to wait at least 70ms before calling readData()
  */
 void Sonar::startReadingCM() {
 	//Serial.println("Ask a reading in centimeters");
 	Sonar::sendCommand(address_, SONAR_COMMAND_REG, SONAR_MODE_CM);
-	//pause (the sonar datasheet recquires 65 ms)
-	delay(70);
+
 }
 
 // Sets maximum range of measurements
-//void Sonar::setMaxRange11m() {
+void Sonar::setMaxRange11m() {
 //max range can be The range is ((Range Register x 43mm) + 43mm) so setting the Range Register to 0 (0x00) gives a maximum
 //range of 43mm. Setting the Range Register to 1 (0x01) gives a maximum range of 86mm. More usefully, 24
 //(0x18) gives a range of 1 metre and 140 (0x8C) is 6 metres. Setting 255 (0xFF) gives the original 11 metres
 //(255 x 43 + 43 is 11008mm). There are two reasons you may wish to reduce the range.
-//sendCommand(SONAR_RANGE_REG, SONAR_RANGE11M); //(0x8C is 6 metres)
-//}
+	sendCommand(address_, SONAR_RANGE_REG, SONAR_RANGE11M); //(0x8C is 6 metres)
+}
 
 // Set to read off the register with stored result
 void Sonar::setRegister(int thisRegister) {
@@ -72,6 +76,8 @@ void Sonar::setRegister(int thisRegister) {
 // Read data from register return result
 int Sonar::readData(int numBytes) {
 	int result = 0;        // the result is two bytes long
+	// set result register for reading
+	setRegister (SONAR_RESULT_REG_HIGH);
 	// send I2C request for data:
 	Wire.requestFrom(address_, numBytes);
 	// wait for two bytes to return:
@@ -79,16 +85,12 @@ int Sonar::readData(int numBytes) {
 		// wait for result
 	}
 	// read the two bytes, and combine them into one int:
-	delay(50);
 	result = Wire.read() * 256;
 	result += Wire.read();
 	// return the result:
 	return result;
 }
-// Optional change Address -
-// NEW_ADDRESS can be set to any of
-// E0, E2, E4, E6, E8, EA, EC, EE
-// F0, F2, F4, F6, F8, FA, FC, FE
+
 // Optional change Address -
 // NEW_ADDRESS can be set to any of
 // E0, E2, E4, E6, E8, EA, EC, EE
@@ -104,21 +106,21 @@ void Sonar::changeAddress(int NEW_ADDRESS) {
 /*
  * Reads the software revision register, if it is 0xFF, then the lecture is still in progress
  */
-//bool Sonar::isReady(void) {
-//	int numBytes = 1;
-//	int result = 0;        // the result is one byte long
-//	// set register for reading
-//	setRegister (SONAR_SOFTWARE_REV_REG);
-//	// send I2C request for data:
-//	Wire.requestFrom(address_, numBytes);
-//	// wait for two bytes to return:
-//	while (Wire.available() < 1) {
-//		// wait for result
-//	}
-//	// read the two bytes, and combine them into one int:
-//	result = Wire.read();
-//	if (result == 0xFF)
-//		return true;
-//	else
-//		return false;
-//}
+bool Sonar::isReady(void) {
+	int numBytes = 1;
+	int result = 0;        // the result is one byte long
+	// set register for reading
+	setRegister (SONAR_SOFTWARE_REV_REG);
+	// send I2C request for data:
+	Wire.requestFrom(address_, numBytes);
+	// wait for two bytes to return:
+	while (Wire.available() < 1) {
+		// wait for result
+	}
+	// read the two bytes, and combine them into one int:
+	result = Wire.read();
+	if (result == 0xFF)
+		return true;
+	else
+		return false;
+}
--- sketchbook_eclipse/srf08/srf08.cpp
+++ sketchbook_eclipse/srf08/srf08.cpp
@@ -5,9 +5,6 @@
  /**********************************
  *  Functions to control the SONAR
  **********************************/
-#define CommandRegister 0x00
-#define ResultRegister  0x02
-int New_Address = 248; //0xF8
 int sensor_reading = 0;
 Sonar sonar;
 /*************************************
@@ -18,15 +15,9 @@ void setup() {
 	while (!Serial) {
 		; // wait for serial port to connect. Needed for Leonardo only
 	}
-	delay(5000); //Wait for some seconds to wait serial monitor
+	delay(2000); //Wait for some seconds to wait serial monitor
 	sonar.connect();
 	Serial.println("SONAR 08");
-	Serial.println("Previous Address:");
-	Serial.println(New_Address);
-	sonar.changeAddress(New_Address);	//Don't delete this!!
-	//Initialize serial and wait for port to open:
-	Serial.println("Address:");
-	Serial.println(New_Address);
 }
 
 /***********************************
@@ -36,22 +27,16 @@ long publisher_timer;
 
 void loop() {
 	if (millis() > publisher_timer) {
-		Serial.println("Address:");
-		Serial.println(New_Address);
 		// step 1: request reading from sensor
 		sonar.startReadingCM();
-
-		// set register for reading
-		sonar.setRegister(ResultRegister);
-
+		//pause (the sonar datasheet recquires 65 ms)
+		delay(70);
 		// read data from result register
 		sensor_reading = sonar.readData(2);
-
 		//Publish to serial monitor
 		Serial.println("Range:");
 		Serial.println(sensor_reading);
 		//publish twice a second (aprox.)
 		publisher_timer = millis() + 500;
-
 	}
 }
