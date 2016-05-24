f40d42ec438abc56f25de7c7c50f86d3c55b5035
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2016-05-23 21:30:43 -0500

    ros_bluetooth adding some comments

--- sketchbook_eclipse/ros_bluetooth/ros_bluetooth.h
+++ sketchbook_eclipse/ros_bluetooth/ros_bluetooth.h
@@ -4,16 +4,20 @@
 // - extern variable definitions
 // In the appropriate section
 
+/*** CHECK THIS FOR ARDUINO UNO OR LEONARDO
+ #ifndef USE_USBCON
+ #define USE_USBCON //Comment this line if you are using Arduino UNO, or leonardo with bluetooth.
+ //Uncomment for normal arduino leonardo
+ */
+
 #ifndef _ros_bluetooth_H_
 #define _ros_bluetooth_H_
-//#define USE_USBCON //Comment this line if you are using Arduino UNO, or leonardo with bluetooth.
+
 #include "Arduino.h"
 #include <ros.h>
 #include <std_msgs/String.h>
 
-
 //add your includes for the project ros_bluetooth here
-
 
 //end of add your includes here
 #ifdef __cplusplus
@@ -26,9 +30,6 @@ void setup();
 #endif
 
 //add your function definitions for the project ros_bluetooth here
-
-
-
 
 //Do not add code below this line
 #endif /* _ros_bluetooth_H_ */
