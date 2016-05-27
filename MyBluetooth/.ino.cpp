48b79e6438a32e9506ccd0b879a1f02baf63df42
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2016-05-26 21:28:53 -0500

    delete Release folder from git index

--- MyBluetooth/.ino.cpp
+++ MyBluetooth/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-05-24 19:36:25
+//This file has been generated on 2016-05-26 21:19:41
 
 #include "Arduino.h"
 #include <Arduino.h>
