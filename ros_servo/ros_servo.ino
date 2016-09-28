c178c3bb299156533e4a664de8523f5b31dd8381
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2016-09-28 14:34:05 -0500

    working with two servo motors, just plug and play version to be used to test the servos

--- ros_servo/ros_servo.ino
+++ ros_servo/ros_servo.ino
@@ -8,7 +8,7 @@
  */
 
 //Classes
-Servo servo;
+Servo servo_yaw,  servo_pitch;
 
 //ROS stuff
 ros::NodeHandle  nh;
@@ -20,8 +20,8 @@ ros::NodeHandle  nh;
  *  cmd_msg:  (std_msgs::UInt16) should have a value from 0-180
  */
 void servo_cb( const std_msgs::UInt16& cmd_msg){
-  servo.write(cmd_msg.data); //set servo angle, should be from 0-180
-  digitalWrite(LED, HIGH-digitalRead(LED));  //toggle led
+  servo_yaw.write(cmd_msg.data); //set servo angle, should be from 0-180
+  //digitalWrite(LED, HIGH-digitalRead(LED));  //toggle led
 }
 
 ros::Subscriber<std_msgs::UInt16> sub("arduino/servo", servo_cb);
@@ -33,11 +33,18 @@ void setup(){
   nh.initNode();
   nh.subscribe(sub);
 
-  servo.attach(SERVO_SIGNAL_PIN); //attach it to the pin where the servo is connected
+  servo_yaw.attach(SERVO_YAW_SIGNAL_PIN); //attach it to the pin where the servo is connected
+  servo_yaw.write(YAW_INITIAL_ANGLE); //set YAW motor at an initial angle  (looking to the front) to add resistance when not in use.
+
+  servo_pitch.attach(SERVO_PITCH_SIGNAL_PIN); //attach it to the pin where the servo is connected
+  servo_pitch.write(PITCH_INITIAL_ANGLE); //set PITCH motor at an initial angle  (looking to the front) to add resistance when not in use.
 }
 
 void loop(){
+  servo_yaw.write(YAW_INITIAL_ANGLE);
+  servo_pitch.write(PITCH_INITIAL_ANGLE); //set PITCH motor at an initial angle  (looking to the front) to add resistance when not in use.
+  digitalWrite(LED, HIGH-digitalRead(LED));
   nh.spinOnce();
-  delay(1);
+  delay(100);
 }
 
