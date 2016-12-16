98675d49b1a55c84eb959cd45ddc8374527090cf
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2016-12-16 10:27:18 -0600

    just some project changes, working!!

--- encoders/.ino.cpp
+++ encoders/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-09-27 20:52:45
+//This file has been generated on 2016-10-27 19:45:34
 
 #include "Arduino.h"
 #include <Encoder.h>
