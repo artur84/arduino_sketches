da19daac808841905992cc9cfef1a543339693fc
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2017-06-05 18:46:35 -0500

    Eclipse configuration files changes

--- iteshu_driver/.ino.cpp
+++ iteshu_driver/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-02-03 17:19:56
+//This file has been generated on 2017-02-03 17:43:23
 
 #include "Arduino.h"
 #include "iteshu_driver.h"
--- motor_pid_control/.ino.cpp
+++ motor_pid_control/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-02-24 10:58:20
+//This file has been generated on 2017-02-24 11:52:01
 
 #include "Arduino.h"
 #include "motor_pid_control.h"
@@ -11,6 +11,7 @@ void loop() ;
 void read_wheel_vel() ;
 double radpsec2rpm(double vel_radpsec) ;
 double radpsec2meterpsec(double vel_radpsec) ;
+double encoder_filter(double new_measurement) ;
 
 #include "motor_pid_control.ino"
 
